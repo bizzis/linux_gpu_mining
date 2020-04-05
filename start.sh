df5a1ee55618ae0802fc1f04d81a207501b5e762
bizzis <63175191+bizzis@users.noreply.github.com>
2020-04-05 04:21:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  eu.nicehash.com --user 359exehpqzBmrPbrNTHiXiuNak925FKYT3.createthis/thanasis.231999@gmail.com --pec --fee 0 --pass z --port 9009
